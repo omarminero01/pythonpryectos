41a5ae8dd92e2442ca2e24a190d1b0b69c097d1a
Omar Minero <omar.minero01@gmail.com>
2025-06-18 17:49:07 -0600

    python updates pruebas

---
+++ pruebas_de_palitos.py
@@ -0,0 +1,39 @@
+#juego de palitos para seleccion de lavar platos 
+from random import shuffle
+
+#lista inicial
+palitos =["-","--","---","----"]
+
+#mezclar palitos
+def mezclar (lista):
+    shuffle(lista)
+    return (lista)
+
+
+#pedirle intento
+def probar_suerte ():
+    intento = " "
+
+    while intento not in ['1','2','3','4']:
+        intento = input("Elige un numero del 1 al 4 :  ")
+
+    return int(intento)
+
+
+
+
+
+#comprobar intento
+
+def chequear_intento(lista,intento):
+    if lista[intento -1] == '-':
+        print("lava los platos")
+    else:
+        print("no lavas platos ")
+
+    print(f"Te ha tocado {lista[intento -1]}")
+
+
+palitos_mezclados = mezclar(palitos)
+seleccion = probar_suerte()
+chequear_intento(palitos_mezclados,seleccion)
