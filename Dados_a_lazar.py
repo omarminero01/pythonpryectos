4473c5611a69fab27307913f01c1f115f4f737d9
Omar Minero <omar.minero01@gmail.com>
2025-06-18 18:38:58 -0600

    Dados

---
+++ Dados_a_lazar.py
@@ -0,0 +1,13 @@
+import random
+ 
+def lanzar_dados():
+    return random.randint(1,6), random.randint(1,6)
+ 
+def evaluar_jugada(dado1, dado2):
+    suma_dados = dado1 + dado2
+    if suma_dados <= 6:
+        return f"La suma de tus dados es {suma_dados}. Lamentable"
+    elif suma_dados > 6 and suma_dados < 10:
+        return f"La suma de tus dados es {suma_dados}. Tienes buenas chances"
+    else:
+        return f"La suma de tus dados es {suma_dados}. Parece una jugada ganadora"
